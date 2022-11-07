4873788bdc50906f54b79b8838d8f903db845e74
Arbit3r <rikukoski2@gmail.com>
2022-11-07 14:46:17 +0200

    package

---
+++ tests/test.js
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+test('test', async ({ page }) => {
+  await page.goto('https://areena.yle.fi/tv/opas');
+  
+  await expect(page.locator('span:has-text("22.00 Kymmenen uutiset")')).toBeTruthy();
+  
+});
