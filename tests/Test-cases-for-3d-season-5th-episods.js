683d9d689c3f32ba8f820c7b99083657408486f9
Vikingi22 <fatlum.gerguri@metropolia.fi>
2022-11-07 15:53:57 +0200

    second test task

--- tests/Test-cases-for-3d-season-5th-episods.js
+++ tests/Test-cases-for-3d-season-5th-episods.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 test('test', async ({ page }) => {
-  await page.goto('https://areena.yle.fi/tv/opas');
+  await page.goto('https://areena.yle.fi/1-3339547');
   
-  await expect(page.locator('span:has-text("22.00 Kymmenen uutiset")')).toBeTruthy();
+  await expect(page.locator('span:has-text("Test cases for finding 3rd season, 5th episodes name and date")')).toBeTruthy();
   
 });
