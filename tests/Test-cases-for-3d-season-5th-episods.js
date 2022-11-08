808f4fabf964b2f70354949d3f9a51dfa2aa6828
Vikingi22 <fatlum.gerguri@gmail.com>
2022-11-09 00:32:07 +0200

    Test case 3d

--- tests/Test-cases-for-3d-season-5th-episods.js
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-test('test', async ({ page }) => {
-  await page.goto('https://areena.yle.fi/1-3339547');
-  
-  await expect(page.locator('span:has-text("Test cases for finding 3rd season, 5th episodes name and date")')).toBeTruthy();
-  
-});
