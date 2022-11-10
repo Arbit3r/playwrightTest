19d476e3686ddb9709c3e263991aba2f2bb1a2b3
Arbit3r <rikukoski2@gmail.com>
2022-11-10 18:33:06 +0200

    LambdaTest

--- tests/Test_cases_for_checking_logos_and_texts.spec.js
+++ tests/Test_cases_for_checking_logos_and_texts.spec.js
@@ -1,11 +1,70 @@
-import { test, expect } from '@playwright/test';
+const { chromium } = require('playwright')
+const { expect } = require('@playwright/test')
 
-test('test', async ({ page }) => {
+const parallelTests = async (capability) => {
+  console.log('Initialising test:: ', capability['LT:Options']['name'])
+
+  const browser = await chromium.connect({
+    wsEndpoint: `wss://cdp.lambdatest.com/playwright?capabilities=${encodeURIComponent(JSON.stringify(capability))}`
+  })
+
+  const page = await browser.newPage()
 
   await page.goto('https://areena.yle.fi/tv/opas');
   await page.getByRole('heading', { name: 'Yle TV1' }).getByRole('link', { name: 'Yle TV1' }).click();
   await expect(page).toHaveURL('https://areena.yle.fi/tv/ohjelmat/yle-tv1');
-});
+
+  await browser.close()
+}
+
+// Capabilities array for with the respective configuration for the parallel tests
+const capabilities = [
+  {
+    'browserName': 'Chrome', // Browsers allowed: `Chrome`, `MicrosoftEdge`, `pw-chromium`, `pw-firefox` and `pw-webkit`
+    'browserVersion': 'latest',
+    'LT:Options': {
+      'platform': 'Windows 10',
+      'build': 'Playwright Sample Build',
+      'name': 'Playwright Sample Test on Windows 10 - Chrome',
+      'user': process.env.LT_USERNAME,
+      'accessKey': process.env.LT_ACCESS_KEY,
+      'network': true,
+      'video': true,
+      'console': true
+    }
+  },
+  {
+    'browserName': 'MicrosoftEdge',
+    'browserVersion': 'latest',
+    'LT:Options': {
+      'platform': 'Windows 8',
+      'build': 'Playwright Sample Build',
+      'name': 'Playwright Sample Test on Windows 8 - MicrosoftEdge',
+      'user': process.env.LT_USERNAME,
+      'accessKey': process.env.LT_ACCESS_KEY,
+      'network': true,
+      'video': true,
+      'console': true
+    }
+  },
+  {
+    'browserName': 'Chrome',
+    'browserVersion': 'latest',
+    'LT:Options': {
+      'platform': 'MacOS Big sur',
+      'build': 'Playwright Sample Build',
+      'name': 'Playwright Sample Test on MacOS Big sur - Chrome',
+      'user': process.env.LT_USERNAME,
+      'accessKey': process.env.LT_ACCESS_KEY,
+      'network': true,
+      'video': true,
+      'console': true
+    }
+  }]
+
+capabilities.forEach(async (capability) => {
+  await parallelTests(capability)
+})
 
 
 
