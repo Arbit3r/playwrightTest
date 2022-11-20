5302195d72fbb983c5f340f36185ff0546200113
Vikingi22 <fatlum.gerguri@gmail.com>
2022-11-20 16:38:49 +0200

    Axe core cod added for testing

--- tests/Test-cases-for-2200-news.js
+++ tests/Test-cases-for-2200-news.js
@@ -1,3 +1,6 @@
+
+//const { axe } = require('@axe-core/playwright').default;
+const AxeBuilder = require('@axe-core/playwright').default;
 const { chromium } = require('playwright')
 const { expect } = require('@playwright/test')
 
@@ -8,12 +11,28 @@ const parallelTests = async (capability) => {
     wsEndpoint: `wss://cdp.lambdatest.com/playwright?capabilities=${encodeURIComponent(JSON.stringify(capability))}`
   })
 
-  const page = await browser.newPage()
+  const context = await browser.newContext();
+  const page = await context.newPage()
+  let testError = 0;
+  try {
+    const builder = new AxeBuilder({ page });
+    builder.options({ reviewOnFail: true });
+    const results = await builder 
+      .analyze()
+      //console.log(" Vialations: " +results.violations.length);
+      //console.log(" Incomplete: " +results.incomplete.length);      
+      //console.log(" Inapplicable: " +results.inapplicable.length);    
+      //console.log(" Passes: " +results.passes.length);
+      //expect(results.violations.length).toEqual([]);
+      testError = results.violations.length + results.inapplicable.length;
+    } catch (e) {
+    console.error('Something bad happened:', e.message);
+  }
 
   await page.goto('https://areena.yle.fi/tv/opas');
 
   await expect(page.locator('span:has-text("22.00 Kymmenen uutiset")')).toBeTruthy();
-
+  console.log("Testing errors: "+ testError);
   await browser.close()
 }
 
